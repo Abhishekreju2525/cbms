fb8a3c8029fdc669fac6d76e8f7a1ec275601511
Iwin Francis <iwintomfrancis@gmail.com>
2022-07-01 16:48:28 +0530

    user details

---
+++ lib/profiles/profilemenu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String text, icon;
+  final VoidCallback? press;
+
+  get kPrimaryColor => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          primary: kPrimaryColor,
+          padding: EdgeInsets.all(20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          backgroundColor: Color(0xFFF5F6F9),
+        ),
+        onPressed: press,
+        child: Row(
+          children: [
+            SizedBox(width: 20),
+            Expanded(child: Text(text)),
+            Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/profiles/profilepic.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/profile.jpg'),
+          ),
+        ],
+      ),
+    );
+  }
+}
